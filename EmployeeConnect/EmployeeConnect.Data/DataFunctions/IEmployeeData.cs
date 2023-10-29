f3ef16fa4dd580f5b0e086d41445cf304dfad147
Elston Misquitta <elston007@gmail.com>
2023-10-29 12:03:05 +0530

    Changes

---
+++ EmployeeConnect/EmployeeConnect.Data/DataFunctions/IEmployeeData.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public interface IEmployeeData
+    {
+        Task<IEnumerable<EmployeeModel>> FetchEmployee(string SearchText);
+    }
+}
---
+++ EmployeeConnect/EmployeeConnect.Data/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DbAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> FetchData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    }
+}
