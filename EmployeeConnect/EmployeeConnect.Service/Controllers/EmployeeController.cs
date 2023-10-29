5907bfd3651eae23f3205663abda1ec5ee2f8bd1
Elston Misquitta <elston007@gmail.com>
2023-10-29 12:15:31 +0530

    Changes

---
+++ EmployeeConnect/EmployeeConnect.Service/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Data;
+using DataAccess.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeConnect.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeData _data;
+
+        public EmployeeController(IEmployeeData data)
+        {
+            _data = data;
+        }
+
+        [HttpGet]
+        [Route("FetchAllEmployees")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(EmployeeModel))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> FetchAllEmployees()
+        {
+            var product = await _data.FetchEmployee("");
+            return product == null ? NotFound() : Ok(product);
+        }
+    }
+}
