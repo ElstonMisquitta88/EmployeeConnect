6e9d1860e401556b522a39392310688e36ad7ed6
Elston Misquitta <elston007@gmail.com>
2023-10-29 12:19:32 +0530

    Changes

--- EmployeeConnect/EmployeeConnect.Service/Program.cs
+++ EmployeeConnect/EmployeeConnect.Service/Program.cs
@@ -1,3 +1,6 @@
+using DataAccess.Data;
+using DataAccess.DbAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddScoped<IEmployeeData, EmployeeData>();
 
 var app = builder.Build();
 
